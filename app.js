6611de571b382c9b250b39402cff4e0e39da2c37
Austin Lilly <austinlilly15@gmail.com>
2020-11-16 13:28:22 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
 const log = console.log;
 
-log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+log(chalk.red.inverse.bold.underline('\t\tObelius'));
+log(chalk.green('\tObelius was a man of many feats.'));
+log(chalk.green.italic('\tHe was known for being the son of Cratos.'));
+log(chalk.green.dim('\tHarboring the strength of physicality.'));
+log(chalk.green.strikethrough('\tThe strength of mentality.'));
+log(chalk.green.dim('\tThe strength of spirituality.'));
+log(chalk.cyan.bold('\tAnd the strength of emotion.'));
+
+chalkAnimation.rainbow('\t-Story by Austin Lilly-', 0.5);
